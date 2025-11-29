47d133ee3aa72b44175eddf7c1ce8176956a1a31
Daniel121826 <alumno709866@ies-azarquiel.es>
2025-11-29 17:23:45 +0100

    prueba-fotos

---
+++ fotos.js
@@ -0,0 +1,124 @@
+const galleryData = [
+    {
+        alt: "Burro deseando que llegue ya el fin de semana",
+        sources: [
+            { media: "(max-width:734px)", srcset: "output-adv/donkey-1062669_1280-small-1x..jpg, output-adv/donkey-1062669_1280-small-2x..jpg" },
+            { media: "(max-width:1068px)", srcset: "output-adv/donkey-1062669_1280-medium-1x..jpg, output-adv/donkey-1062669_1280-medium-2x..jpg" },
+            { media: "(max-width:1440px)", srcset: "output-adv/donkey-1062669_1280-large-1x..jpg, output-adv/donkey-1062669_1280-large-2x..jpg" },
+            { media: "(min-width:0px)", srcset: "output-adv/donkey-1062669_1280-xlarge-1x..jpg, output-adv/donkey-1062669_1280-xlarge-2x..jpg" }
+        ],
+        src: "output-adv/donkey-1062669_1280-xlarge-1x..jpg"
+    },
+    {
+        alt: "Tortuga triste por estar tan sola",
+        sources: [
+            { media: "(max-width:734px)", srcset: "output-adv/green-sea-turtle-8199770_1920-small-1x..jpg, output-adv/green-sea-turtle-8199770_1920-small-2x..jpg" },
+            { media: "(max-width:1068px)", srcset: "output-adv/green-sea-turtle-8199770_1920-medium-1x..jpg, output-adv/green-sea-turtle-8199770_1920-medium-2x..jpg" },
+            { media: "(max-width:1440px)", srcset: "output-adv/green-sea-turtle-8199770_1920-large-1x..jpg, output-adv/green-sea-turtle-8199770_1920-large-2x..jpg" },
+            { media: "(min-width:0px)", srcset: "output-adv/green-sea-turtle-8199770_1920-xlarge-1x..jpg, output-adv/green-sea-turtle-8199770_1920-xlarge-2x..jpg" }
+        ],
+        src: "output-adv/green-sea-turtle-8199770_1920-xlarge-1x..jpg"
+    },
+    {
+        alt: "Hipopotamos contando chistes",
+        sources: [
+            { media: "(max-width:734px)", srcset: "output-adv/hippopotamus-95472_1280-small-1x..jpg, output-adv/hippopotamus-95472_1280-small-2x..jpg" },
+            { media: "(max-width:1068px)", srcset: "output-adv/hippopotamus-95472_1280-medium-1x..jpg, output-adv/hippopotamus-95472_1280-medium-2x..jpg" },
+            { media: "(max-width:1440px)", srcset: "output-adv/hippopotamus-95472_1280-large-1x..jpg, output-adv/hippopotamus-95472_1280-large-2x..jpg" },
+            { media: "(min-width:0px)", srcset: "output-adv/hippopotamus-95472_1280-xlarge-1x..jpg, output-adv/hippopotamus-95472_1280-xlarge-2x..jpg" }
+        ],
+        src: "output-adv/hippopotamus-95472_1280-xlarge-1x..jpg"
+    },
+    {
+        alt: "Leones vagueando en la sabana",
+        sources: [
+            { media: "(max-width:734px)", srcset: "output-adv/lions-1660044_1280-small-1x..jpg, output-adv/lions-1660044_1280-small-2x..jpg" },
+            { media: "(max-width:1068px)", srcset: "output-adv/lions-1660044_1280-medium-1x..jpg, output-adv/lions-1660044_1280-medium-2x..jpg" },
+            { media: "(max-width:1440px)", srcset: "output-adv/lions-1660044_1280-large-1x..jpg, output-adv/lions-1660044_1280-large-2x..jpg" },
+            { media: "(min-width:0px)", srcset: "output-adv/lions-1660044_1280-xlarge-1x..jpg, output-adv/lions-1660044_1280-xlarge-2x..jpg" }
+        ],
+        src: "output-adv/lions-1660044_1280-xlarge-1x..jpg"
+    },
+    {
+        alt: "Loros dandose el lote",
+        sources: [
+            { media: "(max-width:734px)", srcset: "output-adv/parrots-3427188_640-small-1x..jpg, output-adv/parrots-3427188_640-small-2x..jpg" },
+            { media: "(max-width:1068px)", srcset: "output-adv/parrots-3427188_640-medium-1x..jpg, output-adv/parrots-3427188_640-medium-2x..jpg" },
+            { media: "(max-width:1440px)", srcset: "output-adv/parrots-3427188_640-large-1x..jpg, output-adv/parrots-3427188_640-large-2x..jpg" },
+            { media: "(min-width:0px)", srcset: "output-adv/parrots-3427188_640-xlarge-1x..jpg, output-adv/parrots-3427188_640-xlarge-2x..jpg" }
+        ],
+        src: "output-adv/parrots-3427188_640-xlarge-1x..jpg"
+    },
+    {
+        alt: "Pelicano pensando en el proximo examen de DIW",
+        sources: [
+            { media: "(max-width:734px)", srcset: "output-adv/pelican-7551248_1280-small-1x..jpg, output-adv/pelican-7551248_1280-small-2x..jpg" },
+            { media: "(max-width:1068px)", srcset: "output-adv/pelican-7551248_1280-medium-1x..jpg, output-adv/pelican-7551248_1280-medium-2x..jpg" },
+            { media: "(max-width:1440px)", srcset: "output-adv/pelican-7551248_1280-large-1x..jpg, output-adv/pelican-7551248_1280-large-2x..jpg" },
+            { media: "(min-width:0px)", srcset: "output-adv/pelican-7551248_1280-xlarge-1x..jpg, output-adv/pelican-7551248_1280-xlarge-2x..jpg" }
+        ],
+        src: "output-adv/pelican-7551248_1280-xlarge-1x..jpg"
+    },
+    {
+        alt: "Ardilla planenado su proximo movimiento",
+        sources: [
+            { media: "(max-width:734px)", srcset: "output-adv/squirrel-7678830_1280-small-1x..jpg, output-adv/squirrel-7678830_1280-small-2x..jpg" },
+            { media: "(max-width:1068px)", srcset: "output-adv/squirrel-7678830_1280-medium-1x..jpg, output-adv/squirrel-7678830_1280-medium-2x..jpg" },
+            { media: "(max-width:1440px)", srcset: "output-adv/squirrel-7678830_1280-large-1x..jpg, output-adv/squirrel-7678830_1280-large-2x..jpg" },
+            { media: "(min-width:0px)", srcset: "output-adv/squirrel-7678830_1280-xlarge-1x..jpg, output-adv/squirrel-7678830_1280-xlarge-2x..jpg" }
+        ],
+        src: "output-adv/squirrel-7678830_1280-xlarge-1x..jpg"
+    },
+    {
+        alt: "Rana que te juzga",
+        sources: [
+            { media: "(max-width:734px)", srcset: "output-adv/waters-3038803_1280-small-1x..jpg, output-adv/waters-3038803_1280-small-2x..jpg" },
+            { media: "(max-width:1068px)", srcset: "output-adv/waters-3038803_1280-medium-1x..jpg, output-adv/waters-3038803_1280-medium-2x..jpg" },
+            { media: "(max-width:1440px)", srcset: "output-adv/waters-3038803_1280-large-1x..jpg, output-adv/waters-3038803_1280-large-2x..jpg" },
+            { media: "(min-width:0px)", srcset: "output-adv/waters-3038803_1280-xlarge-1x..jpg, output-adv/waters-3038803_1280-xlarge-2x..jpg" }
+        ],
+        src: "output-adv/waters-3038803_1280-xlarge-1x..jpg"
+    }
+];
+
+const gallery = document.getElementById('galeria');
+
+galleryData.forEach(item => {
+    const div = document.createElement('div');
+    div.className = 'galeria-item';
+
+
+    const picture = document.createElement('picture');
+    item.sources.forEach(sourceData => {
+        const source = document.createElement('source');
+        source.media = sourceData.media;
+        source.srcset = sourceData.srcset;
+        picture.appendChild(source);
+    });
+
+    const img = document.createElement('img');
+    img.src = item.src;
+    img.alt = item.alt;
+
+    picture.appendChild(img);
+    div.appendChild(picture);
+
+    // Descripcion de la foto
+    const descripcion = document.createElement('div');
+    descripcion.className = 'descripcion';
+    descripcion.innerText = item.alt;
+    div.appendChild(descripcion);
+
+    gallery.appendChild(div);
+
+    // Ventana modal
+    div.addEventListener('click', () => {
+        document.getElementById('modal').style.display = "block";
+        document.getElementById('modal-img').src = item.src;
+        document.getElementById('modal-descripcion').innerText = item.alt;
+    });
+});
+//Cerrar ventana modal
+document.querySelector('.close').onclick = function () {
+    document.getElementById('modal').style.display = "none";
+}
